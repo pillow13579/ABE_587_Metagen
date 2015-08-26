5e261e7ed3eb697542fe67091d5622439b6ec0b3
xiangl1 <xiangl1@service1.ice.americas.sgi.com>
2015-08-26 14:21:58 -0700

    week2

---
+++ week2_problem/perl1_problem.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+# Q2:Print out "Hello Tucson"
+print "Hello Tucson!\n";
+
+# Q3:Store my name in a variable and print the contents
+my $name = 'Xiang Liu';
+
+print $name,"\n";
+
+# Q4:Statement with tabs and newlines;
+print "I like Metagenomics.\t$name\n";
+
+# Q5:Statement with double quotes
+print "This is a fun class! 2+3\t$name\n";
+
+# Q6:Statement with single quotes
+print 'This is a fun class! 2+3\t$name\n', "\n";
+
+# Q6: print 5 instead of 2+3
+print "2+3 = ", 2+3, "\n";
+
+# Q7: The difference between single and double quotes is that, double quotes explain 
+# variables and special characters but single quotes don't.
+
+exit;
