d1d9f98bc886197bf15940ed063b92685d4a5ca4
xiangl1 <xiangl1@service2.ice.americas.sgi.com>
2015-09-08 09:36:21 -0700

    week2

--- week2_problem/q7.pl
+++ week2_problem/q7.pl
@@ -2,8 +2,6 @@
 use strict;
 use warnings;
 
-my $name = 'Xiang';
-
 # Q7: statement with single quotes
 print "The difference between single and double quotes is that,
 double quotes explain variables and special characters but single quotes don't.\n";
